ad1e2427abd36733352557be6947d7d40bd27f16
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-04-02 23:33:41 +0900

    Update iris_matrix.py

--- iris_matrix.py
+++ iris_matrix.py
@@ -35,3 +35,24 @@ tot = sum(pca.explained_variance_ratio_)
 print("寄与率:", str(["{:.1f}%".format(val * 100/tot) for val in 
                    pca.explained_variance_ratio_]))
                    
+import matplotlib.pyplot as plt
+pca = PCA(n_components=2)
+pca.fit(x_train_std)
+x_train_pca = pca.transform(x_train_std)
+
+def show_scatter(x, y):
+    markers = ["s","x","v"]
+    for idx, label in enumerate(np.unique(y)):
+        plt.scatter(
+            x=x[y == label, 0],
+            y=x[y == label, 1],
+            marker = markers[idx],
+            label = label
+        )
+    plt.grid()
+    plt.xlabel("pc1")
+    plt.ylabel("pc2")
+    plt.show()
+        
+
+show_scatter(x_train_pca, y_train)
