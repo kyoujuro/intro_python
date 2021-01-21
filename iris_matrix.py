69420d4c3c5921f5cad075a95105e41e26537cd1
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2021-01-21 23:26:21 +0900

    因子分析も追加

--- iris_matrix.py
+++ iris_matrix.py
@@ -35,3 +35,15 @@ tot = sum(pca.explained_variance_ratio_)
 print("寄与率:", str(["{:.1f}%".format(val * 100/tot) for val in 
                    pca.explained_variance_ratio_]))
                    
+# sklearnのFactorAnalysis(因子分析)クラスをインポート
+from sklearn.decomposition import FactorAnalysis as FA
+
+# 因子数を指定
+n_components=3
+
+# 因子分析の実行
+fa = FA(n_components, max_iter=5000) # モデルを定義
+fitted = fa.fit_transform(z) # fitとtransformを一括処理
+
+print(fitted)
+print(fitted.shape)
