9f4ed4cefb9d58322e899687bd653d488b89c7b4
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2022-01-06 22:14:49 +0900

    add

--- pubchem_pra.py
+++ pubchem_pra.py
@@ -1,7 +1,17 @@
 import pubchempy as pcp
-
+import pandas as pd
 properties = ['MolecularFormula', 'MolecularWeight', 'CanonicalSMILES', 'IUPACName']
 
 result = pcp.get_properties(properties, '64-14-5', 'name')
+properties = ['MolecularFormula','CanonicalSMILES', 'IUPACName']
+cas_list = ['100-21','1002-62','1002-15-6']
 
 print(result)
+df = pd.DataFrame()
+for cas in cas_list:
+    try:
+        temp = pcp.get_properties(properties, cas, 'name',as_dataframe=True)
+        temp['CAS'] = cas
+        df = pd.concat([df,temp],axis=0,join='outer',sort=True)
+    except:
+        pass
