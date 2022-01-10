2ed9f62ec1454c549e5ecd53f8276c38d50df267
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2022-01-10 21:07:59 +0900

    add

--- defined_pension.py
+++ defined_pension.py
@@ -1,8 +1,9 @@
 class Present_value:
-    def __init__(self, W, b):
-        self.W = W
-        self.b = b
+    def __init__(self, an, i):
+        self.an = an
+        self.i = i
 
     def risoku(an, sn):
         return 1 / an + 1 / sn
-        
+    def genka(self):
+        return self.an * (1 + self.i)
