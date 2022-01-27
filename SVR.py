be54287ee83f3338f1b579e967d89135eb7ce37a
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2022-01-27 21:56:53 +0900

    add

--- SVR.py
+++ SVR.py
@@ -1,3 +1,10 @@
 import numpy as np
-from sklearn.svm import SVR
 import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+from sklearn.model_selection import GridSearchCV
+
+boston = load_boston()
