7550f72b74998c70fba141818956870894e20681
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2022-01-29 22:21:59 +0900

    add

--- SVR.py
+++ SVR.py
@@ -9,9 +9,10 @@ from sklearn.model_selection import GridSearchCV
 
 boston = load_boston()
 X, Xtest, y, ytest = train_test_split(boston['data'], boston['target'], test_size=0.25)
-def gen_cv():
+def generate_crossvalidation():
     m_train = np.floor(len(y)*0.75).astype(int)
     train_indices = np.arange(m_train)
     test_indices = np.arange(m_train, len(y))
     yield (train_indices, test_indices)
-
+scaler = StandardScaler()
+X_norm = scaler.fit_transform(X)
